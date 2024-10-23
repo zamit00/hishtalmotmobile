967086e66dc80e8a743f869d1fc4b15482b7e051
zamit00 <zamit00@gmail.com>
2024-10-23 17:25:08 +0300

    Add files via upload

---
+++ miktsoeejvs.js
@@ -0,0 +1,10 @@
+function tshuva(k){
+    
+    const text = document.getElementById(k).innerText;
+    const pElement = document.getElementById('teshu');
+    pElement.innerText=text;
+}
+function gotohome(){
+    window.location.href = 'https://zamit00.github.io/MyWebSite/';
+   
+}
